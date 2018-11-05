ff4ee3205a323146f0452e3b5b527006148135e0
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-11-04 22:18:03 -0600

    successful band response

--- liri.js
+++ liri.js
@@ -1,2 +1,15 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
+const artist = process.argv.slice(2).join("%20");
+//   .split(" ")
+//   .join("%");
+console.log(artist);
+const request = require("request");
+request(
+  "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp",
+  (error, response, body) => {
+    console.log("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    console.log("body:", body); // Print the HTML for the Google homepage.
+  }
+);
